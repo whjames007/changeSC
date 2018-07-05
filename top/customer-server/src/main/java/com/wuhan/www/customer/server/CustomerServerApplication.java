f922e6ed404eef954fcab8f528f4f7cfe33b0aeb
James Song <james.song007@hotmail.com>
2018-07-05 13:38:13 +0800

    顶级提交

---
+++ top/customer-server/src/main/java/com/wuhan/www/customer/server/CustomerServerApplication.java
@@ -0,0 +1,14 @@
+package com.wuhan.www.customer.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CustomerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServerApplication.class, args);
+	}
+}
