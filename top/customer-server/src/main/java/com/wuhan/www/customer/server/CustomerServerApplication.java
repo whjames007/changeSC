453fbee72e90fb9093828f6f0fb5294f686423c5
James Song <james.song007@hotmail.com>
2018-07-05 15:36:52 +0800

    222

--- top/customer-server/src/main/java/com/wuhan/www/customer/server/CustomerServerApplication.java
+++ top/customer-server/src/main/java/com/wuhan/www/customer/server/CustomerServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class CustomerServerApplication {
-
+//客户服务
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServerApplication.class, args);
 	}
